63f90dff461f057e563ba5667d3246413209b057
Andrewy-gh <andy.yu617@gmail.com>
2026-02-12 22:40:02 -0500

    feat: scaffold login, sessions, progress reports, and credit transactions routes

---
+++ app/credit-transactions/page.tsx
@@ -0,0 +1,8 @@
+export default function CreditTransactionsPage() {
+  // based on user role we will either filter or display all credit transactions
+  return (
+    <main>
+      <h1>Credit Transactions Page</h1>
+    </main>
+  );
+}

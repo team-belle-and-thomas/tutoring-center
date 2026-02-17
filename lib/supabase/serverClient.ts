f9aa00814f4746d2f623d8b5f44509dee7498f97
Scott <scott@allthingslinux.org>
2026-02-17 09:25:51 -0500

    fix: fixed issue with npm build due to await not showing up in the serverClient.ts file for supabase

--- lib/supabase/serverClient.ts
+++ lib/supabase/serverClient.ts
@@ -1,10 +1,10 @@
-import "server-only"
-import { createServerClient } from "@supabase/ssr"
-import { cookies } from "next/headers"
-import type { Database } from "./types"
+import 'server-only';
+import { cookies } from 'next/headers';
+import { createServerClient } from '@supabase/ssr';
+import type { Database } from './types';
 
-export function createSupabaseServerClient() {
-  const cookieStore = cookies()
+export async function createSupabaseServerClient() {
+  const cookieStore = await cookies();
 
   return createServerClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
@@ -12,19 +12,18 @@ export function createSupabaseServerClient() {
     {
       cookies: {
         getAll() {
-          return cookieStore.getAll()
+          return cookieStore.getAll();
         },
         setAll(cookiesToSet) {
           try {
             cookiesToSet.forEach(({ name, value, options }) => {
-              cookieStore.set(name, value, options)
-            })
+              cookieStore.set(name, value, options);
+            });
           } catch {
             // Nothing here to catch yet, this can be handled in other components
           }
         },
       },
     }
-  )
+  );
 }
-

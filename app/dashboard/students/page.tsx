3298f6a8df29e46943211d363cf97271e1522367
Andrewy-gh <andy.yu617@gmail.com>
2026-02-16 20:38:10 -0500

    feat: add parents/students/tutors routes

---
+++ app/dashboard/students/page.tsx
@@ -0,0 +1,12 @@
+import { getUserRole } from '../mock-api';
+
+export default async function StudentsPage() {
+  const role = await getUserRole();
+  // based on user role we will either filter or display all students
+  return (
+    <main>
+      <h1>Students Page</h1>
+      <p>You are logged in as {role}</p>
+    </main>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,5 @@
 import type { Metadata } from 'next';
 import './globals.css';
-import { Inter } from 'next/font/google';
-
-const inter = Inter({ subsets: ['latin'], variable: '--font-sans' });
 
 export const metadata: Metadata = {
   title: 'Tutoring Center',
@@ -15,7 +12,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en' className={inter.variable}>
+    <html lang='en'>
       <body>{children}</body>
     </html>
   );

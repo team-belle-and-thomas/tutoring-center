5e765c27e6a4e6f91a932cc6caae7ad8c7dd8c6e
Andrewy-gh <andy.yu617@gmail.com>
2026-02-18 17:26:23 -0500

    chore: update imports for data-table.tsx

--- app/dashboard/(admin)/parents/page.tsx
+++ app/dashboard/(admin)/parents/page.tsx
@@ -1,11 +1,11 @@
+import { DataTable } from '@/components/data-table';
 import { getParents } from '@/lib/mock-api';
 import { columns } from './columns';
-import { DataTable } from './data-table';
 
 export default async function ParentsPage() {
   const data = await getParents();
   return (
-    <div className='p-8'>
+    <div className='p-2 md:p-8'>
       <DataTable columns={columns} data={data} />
     </div>
   );

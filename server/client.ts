59a2f22e20a070639d0495797e3158bb35ea2bd6
brady <bradythomas690@gmail.com>
2026-02-18 13:54:45 -0600

    feat: made function to get parent's balance

--- server/client.ts
+++ server/client.ts
@@ -1,9 +1,13 @@
-import { createClient } from '@supabase/supabase-js'
-import dotenv from 'dotenv'
+import { createClient } from '@supabase/supabase-js';
+import dotenv from 'dotenv';
+import { Database } from '../types/supabase';
 
-dotenv.config()
+dotenv.config();
 
-const client = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!)
+const client = createClient<Database>(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+);
 console.log('Supabase client created successfully');
 
 export default client;

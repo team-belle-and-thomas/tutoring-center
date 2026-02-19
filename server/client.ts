5d0f8d85c66a58c1d9d00ad4fa5f545881dabf41
brady <bradythomas690@gmail.com>
2026-02-19 16:38:50 -0600

    fix: deducted_credits after placing successful session

--- server/client.ts
+++ server/client.ts
@@ -4,10 +4,7 @@ import { Database } from '../types/supabase';
 
 dotenv.config();
 
-const client = createClient<Database>(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-);
+const client = createClient<Database>(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.SUPABASE_SERVICE_ROLE_KEY!);
 console.log('Supabase client created successfully');
 
 export default client;

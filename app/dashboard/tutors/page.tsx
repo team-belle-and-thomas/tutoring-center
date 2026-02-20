aa0d2dc228357cdb5a03919d1a60ce6ec5ab475c
paul vinueza <paulvinueza30@gmail.com>
2026-02-20 18:32:43 -0500

    feat: tutors page for all tutors lists basic info with option to view individual tutors

--- app/dashboard/tutors/page.tsx
+++ app/dashboard/tutors/page.tsx
@@ -1,17 +1,19 @@
+import { DM_Sans } from 'next/font/google';
 import { DataTable } from '@/components/data-table';
 import { getTutors, getUserRole } from '@/lib/mock-api';
 import { Tutor } from '@/lib/mock-data';
 import { columns } from './columns';
 
+const dm_sans = DM_Sans({ subsets: ['latin'] });
 export default async function TutorsPage() {
   const role = await getUserRole();
   const data = await getTutors();
   return (
-    <main>
+    <main className={dm_sans.className}>
       <h1>Tutors Page</h1>
       {/* Currently being used for debugging, we will consider removing this later */}
       <p>You are logged in as {role}</p>
-      {role === 'admin' ? <AdminTutorsPage tutors={data} /> : <p>TBD</p>}
+      {role === 'admin' && <AdminTutorsPage tutors={data} />}
     </main>
   );
 }

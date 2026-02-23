ccf1187b50860b9bd59a55e65c32669a58e818dc
Andrewy-gh <andy.yu617@gmail.com>
2026-02-22 19:27:39 -0500

    chore: create mock available sessions

---
+++ lib/mock-available-sessions.ts
@@ -0,0 +1,33 @@
+export type AvailableSession = {
+  scheduled_at: string;
+  ends_at: string;
+};
+
+export const mockAvailableSessions: AvailableSession[] = [
+  { scheduled_at: '2026-02-22T19:00:00Z', ends_at: '2026-02-22T20:00:00Z' },
+  { scheduled_at: '2026-02-22T21:00:00Z', ends_at: '2026-02-22T22:00:00Z' },
+  { scheduled_at: '2026-02-23T19:00:00Z', ends_at: '2026-02-23T20:00:00Z' },
+  { scheduled_at: '2026-02-23T20:00:00Z', ends_at: '2026-02-23T21:00:00Z' },
+  { scheduled_at: '2026-02-23T21:00:00Z', ends_at: '2026-02-23T22:00:00Z' },
+  { scheduled_at: '2026-02-24T20:00:00Z', ends_at: '2026-02-24T21:00:00Z' },
+  { scheduled_at: '2026-02-24T21:00:00Z', ends_at: '2026-02-24T22:00:00Z' },
+  { scheduled_at: '2026-02-24T22:00:00Z', ends_at: '2026-02-24T23:00:00Z' },
+  { scheduled_at: '2026-02-25T19:00:00Z', ends_at: '2026-02-25T20:00:00Z' },
+  { scheduled_at: '2026-02-25T20:00:00Z', ends_at: '2026-02-25T21:00:00Z' },
+  { scheduled_at: '2026-02-26T20:00:00Z', ends_at: '2026-02-26T21:00:00Z' },
+  { scheduled_at: '2026-02-26T21:00:00Z', ends_at: '2026-02-26T22:00:00Z' },
+  { scheduled_at: '2026-02-27T19:00:00Z', ends_at: '2026-02-27T20:00:00Z' },
+  { scheduled_at: '2026-02-27T21:00:00Z', ends_at: '2026-02-27T22:00:00Z' },
+  { scheduled_at: '2026-02-28T20:00:00Z', ends_at: '2026-02-28T21:00:00Z' },
+  { scheduled_at: '2026-02-28T22:00:00Z', ends_at: '2026-02-28T23:00:00Z' },
+  { scheduled_at: '2026-03-01T19:00:00Z', ends_at: '2026-03-01T20:00:00Z' },
+  { scheduled_at: '2026-03-01T21:00:00Z', ends_at: '2026-03-01T22:00:00Z' },
+  { scheduled_at: '2026-03-02T19:00:00Z', ends_at: '2026-03-02T20:00:00Z' },
+  { scheduled_at: '2026-03-02T20:00:00Z', ends_at: '2026-03-02T21:00:00Z' },
+  { scheduled_at: '2026-03-03T20:00:00Z', ends_at: '2026-03-03T21:00:00Z' },
+  { scheduled_at: '2026-03-03T22:00:00Z', ends_at: '2026-03-03T23:00:00Z' },
+  { scheduled_at: '2026-03-04T19:00:00Z', ends_at: '2026-03-04T20:00:00Z' },
+  { scheduled_at: '2026-03-04T21:00:00Z', ends_at: '2026-03-04T22:00:00Z' },
+  { scheduled_at: '2026-03-05T20:00:00Z', ends_at: '2026-03-05T21:00:00Z' },
+  { scheduled_at: '2026-03-05T22:00:00Z', ends_at: '2026-03-05T23:00:00Z' },
+];
